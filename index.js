a27c0643ced8eeb5e5afc92035650c5ce99004ab
skipper <skipper@rimbahouse.com>
2024-04-20 00:17:33 +0700

    Cors

--- index.js
+++ index.js
@@ -5,47 +5,13 @@ const cors = require("cors");
 const http = require("http");
 
 const app = express();
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server, {
-  cors: {
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        var msg =
-          "The CORS policy for this site does not " +
-          "allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
-    methods: ["GET", "POST"],
-  },
-});
-
-require("./socket")(io);
 
 const router = require("./routes/index");
 
 const PORT = process.env.PORT || 5000;
 const public = __dirname + "/public/";
 
-const allowedOrigins = ["http://localhost:3000"];
-
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        var msg =
-          "The CORS policy for this site does not " +
-          "allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
+app.use(cors());
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use("/public/uploads", express.static(path.join(public, "uploads")));
@@ -56,4 +22,4 @@ app.get("/", function (req, res) {
   res.sendFile(path.join(public + "index.html"));
 });
 
-server.listen(PORT, () => console.log(`Server Running on PORT ${PORT}`));
+app.listen(PORT, () => console.log(`Server Running on PORT ${PORT}`));
